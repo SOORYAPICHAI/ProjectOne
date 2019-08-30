aeaefd07dfdb06fe303504e1dac3c91adda6e742
JAYASOORYA M R <JAYASOORYA>
2019-08-30 13:56:57 +0530

    Updates

---
+++ precache-manifest.37adfed53aa8b8e81d4ba18c702c0c5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b7e57eb296210d7ed95af6594ed6fc",
+    "url": "/USA-Product/index.html"
+  },
+  {
+    "revision": "203db50feee8363aeaef",
+    "url": "/USA-Product/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a730d5c2f1f8fd2b755",
+    "url": "/USA-Product/static/js/2.65e324cd.chunk.js"
+  },
+  {
+    "revision": "203db50feee8363aeaef",
+    "url": "/USA-Product/static/js/main.87115485.chunk.js"
+  },
+  {
+    "revision": "119b55d2fb559930e269",
+    "url": "/USA-Product/static/js/runtime~main.bf56f178.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/USA-Product/static/media/logo.5d5d9eef.svg"
+  }
+]);
