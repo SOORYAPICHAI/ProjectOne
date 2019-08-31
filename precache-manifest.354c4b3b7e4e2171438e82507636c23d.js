ae8202e0c4be3b426b38bd5b353ad442351ec62a
JAYASOORYA M R <JAYASOORYA>
2019-08-31 13:45:07 +0530

    Updates

--- precache-manifest.354c4b3b7e4e2171438e82507636c23d.js
+++ precache-manifest.354c4b3b7e4e2171438e82507636c23d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38b9bc5422df3b9eddc88dbde27955e1",
+    "revision": "7b4cd5d148116381a49eb77198de5014",
     "url": "/USA-Product/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/USA-Product/static/css/2.83acd176.chunk.css"
   },
   {
-    "revision": "7833878c460ed2e03098",
+    "revision": "13d178d95168c7a66964",
     "url": "/USA-Product/static/css/main.dcfaaa49.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/USA-Product/static/js/2.2b063701.chunk.js"
   },
   {
-    "revision": "7833878c460ed2e03098",
-    "url": "/USA-Product/static/js/main.4be4a683.chunk.js"
+    "revision": "13d178d95168c7a66964",
+    "url": "/USA-Product/static/js/main.abc02d1b.chunk.js"
   },
   {
     "revision": "119b55d2fb559930e269",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/USA-Product/precache-manifest.815c8ea191a4337d3973429273632f4a.js"
+  "/USA-Product/precache-manifest.354c4b3b7e4e2171438e82507636c23d.js"
 );
 
 self.addEventListener('message', (event) => {
